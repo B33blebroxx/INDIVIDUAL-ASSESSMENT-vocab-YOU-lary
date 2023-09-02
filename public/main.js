63691801f23d6cd8dc916919bd48a3fdb5b9883f
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-09-02 13:55:04 -0500

    setting up basic stuff

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // USE WITH FIREBASE AUTH
-// import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
@@ -10,8 +10,8 @@ const init = () => {
     <button class="btn btn-danger" id="click-me">Click ME!</button><br />
     <hr />
     <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
+    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>`;
+
   console.warn('YOU ARE UP AND RUNNING!');
 
   document
@@ -19,7 +19,7 @@ const init = () => {
     .addEventListener('click', () => console.warn('You clicked that button!'));
 
   // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
+  ViewDirectorBasedOnUserAuthStatus();
 };
 
 init();
