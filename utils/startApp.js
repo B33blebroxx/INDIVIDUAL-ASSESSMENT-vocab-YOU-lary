07a1f340fe36876b118d2a4e117da8e1b5062382
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-09-06 18:22:56 -0500

    Got login/logout buttons working, navbar created but not functional yet, and cards are rendering to the DOM

--- utils/startApp.js
+++ utils/startApp.js
@@ -1,13 +1,15 @@
 import getTerms from '../api/termData';
-import domBuilder from '../shared/domBuilder';
 import logoutButton from '../components/logoutButton';
 import { emptyTerms, showTerms } from '../pages/showTerms';
-// import clearDom from './clearDom';
+import navBar from '../components/shared/navBar';
+import domBuilder from '../components/shared/domBuilder';
 
 const startApp = (user) => {
   domBuilder();
+  navBar();
   logoutButton();
 
+  console.warn(user);
   getTerms(user.uid).then((array) => {
     if (array.length) {
       showTerms(array);
@@ -17,4 +19,4 @@ const startApp = (user) => {
   });
 };
 
-export default startApp();
+export default startApp;
--- utils/viewDirector.js
+++ utils/viewDirector.js
@@ -1,17 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import loginButton from '../components/loginButton';
-import logoutButton from '../components/logoutButton';
 import client from './client';
 import startApp from './startApp';
 
 const ViewDirectorBasedOnUserAuthStatus = () => {
   firebase.initializeApp(client);
   firebase.auth().onAuthStateChanged((user) => {
+    console.warn(user);
     if (user) {
       // person is logged in do something...
       startApp(user);
-      logoutButton();
     } else {
       // person is NOT logged in
       loginButton();
